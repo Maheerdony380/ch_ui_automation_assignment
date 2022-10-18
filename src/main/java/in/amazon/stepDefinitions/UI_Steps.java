b619d3a289f9ed2364d13a19ff56118c02d5eaa8
Maheerdony380 <maheeralam380@gmail.com>
2022-10-18 15:10:32 -0400

    stepDefs added

--- src/main/java/in/amazon/stepDefinitions/UI_Steps.java
+++ src/main/java/in/amazon/stepDefinitions/UI_Steps.java
@@ -1,10 +1,12 @@
 package in.amazon.stepDefinitions;
 
+import in.amazon.pages.AmazonPages;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class UI_Steps {
+    AmazonPages amazonPages = new AmazonPages();
 
     @Given("As a user I Navigate to baseURL")
     public void iNavigateToBaseUrl() {
@@ -13,26 +15,36 @@ public class UI_Steps {
 
     @When("Click on the hamburger menu in the top left corner")
     public void clickOnHamburgerMenu() {
+        amazonPages.clickOnHamburgerMenu();
     }
 
     @When("Scroll down and then Click on the TV, Appliances and Electronics link under Shop by Department section")
     public void scrollAndClickOnTvAppliancesElectronics() {
+        amazonPages.clickOnTVAppliancesElectronics();
     }
 
     @When("Click on Televisions under Tv, Audio & Cameras sub section")
     public void clickOnTelevisions() {
+        amazonPages.clickOnTelevisionsList();
     }
 
     @When("Scroll down and filter the results by Brand {string}")
-    public void scrollAndFilterResultsByBrand(String string) {
+    public void scrollAndFilterResultsByBrand(String string) throws InterruptedException {
+        Thread.sleep(3000);
+        amazonPages.clickOnSamsungBrand();
     }
 
     @When("Sort the {string} results with price {string}")
-    public void sortResultsWithPrice(String string, String string2) {
+    public void sortResultsWithPrice(String string, String string2) throws InterruptedException {
+        Thread.sleep(3000);
+        amazonPages.clickOnSortDropdown();
+        amazonPages.clickOnPriceHighToLow();
     }
 
     @When("Click on the second highest priced item")
     public void clickOnSecondHighestPricedItem() {
+        System.out.println("clickOnSecondHighestPricedItem()");
+        amazonPages.clickOnSecondItem();
     }
 
     @When("Switch the Window")
